2434cc09c6406657eaf39e8e9577af826ec786b3
Zahid Hasan <megamind.d.evilll@gmail.com>
2024-06-29 10:04:07 +0600

    Increment UserDefault with label Update.

--- UserDefaultTests/ViewController.swift
+++ UserDefaultTests/ViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private(set) var counterLabel: UILabel!
+    @IBOutlet private(set) var incrementButton: UIButton!
+    
+    private var count: Int = 0{
+        didSet {
+            self.counterLabel.text = "\(count)"
+            UserDefaults.standard.setValue(count, forKey: "count")
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        count = UserDefaults.standard.integer(forKey: "count")
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction private func incrementButtonTapped(_ sender: UIButton){
+        count += 1
     }
 
 
